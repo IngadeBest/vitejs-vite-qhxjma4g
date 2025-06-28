f616c8c539a3d5069ed715d8c8d3e44d001b2db2
IngadeBest <inniwinnie@live.nl>
2025-06-28 13:14:37 +0000

    fix: supabase named export

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://hpfixcuxayjkfyhqbbnn.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwZml4Y3V4YXlqa2Z5aHFiYm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4ODEyOTEsImV4cCI6MjA2NjQ1NzI5MX0.VFaO3lzLbxHAmNOrHjyKOd3q3Pa_4buRcNSosq3FZDQ'
+export const supabase = createClient(supabaseUrl, supabaseKey);
