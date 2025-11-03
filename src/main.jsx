79cbb4135d4b626654da60b58229fac8246cd47b
IngadeBest <inniwinnie@live.nl>
2025-11-03 12:24:24 +0000

    revert: remove temporary SW-unregister snippet

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,6 @@ import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary";
 import "./ui/tokens.css";
 import "./index.css"; 
-
-// tijdelijk: force-unregister any existing service workers so clients fetch fresh assets
-if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-	try {
-		navigator.serviceWorker.getRegistrations().then(regs => {
-			regs.forEach(reg => {
-				reg.unregister().catch(() => { /* ignore */ });
-			});
-		});
-	} catch (e) {
-		// best-effort cleanup; ignore errors
-		// eslint-disable-next-line no-console
-		console.warn('SW unregister failed', e);
-	}
-}
-
 const rootEl = document.getElementById("root");
 createRoot(rootEl).render(
 	<ErrorBoundary>
